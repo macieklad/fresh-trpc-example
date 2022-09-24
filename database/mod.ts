27ffbeae2e0560cd6820952cf6929027719d792e
Maciej Ładoś <maciej@lados.dev>
2022-09-24 12:57:56 +0200

    chore: log in prod

--- database/mod.ts
+++ database/mod.ts
@@ -6,27 +6,29 @@ export type User = {
   bio?: string;
 };
 
+console.log(Deno.env.get("UPSTASH_URL"), Deno.env.get("UPSTASH_TOKEN"))
+
 const redis = new Redis({
   url: Deno.env.get("UPSTASH_URL") || "",
-  token: Deno.env.get("UPSTASH_TOKEN") || "",
-});
+  token: Deno.env.get("UPSTASH_TOKEN") || ""
+})
 
 export async function createUser(user: User) {
-  const currentUsers = await fetchUsers();
+  const currentUsers = await fetchUsers()
   if (currentUsers.size >= 50) {
-    const [firstKey] = currentUsers.keys();
-    currentUsers.delete(firstKey);
+    const [firstKey] = currentUsers.keys()
+    currentUsers.delete(firstKey)
   }
 
-  currentUsers.set(user.id, user);
-  await redis.set("users", Object.fromEntries(currentUsers));
-}
+  currentUsers.set(user.id, user)
+  await redis.set("users", Object.fromEntries(currentUsers))
+} 
 
 export async function fetchUsers(): Promise<Map<string, User>> {
-  const currentUsers = await redis.get<Record<string, User>>("users") || {};
-  return new Map(Object.entries(currentUsers));
-}
+  const currentUsers = await redis.get<Record<string, User>>("users") || {}
+  return new Map(Object.entries(currentUsers))
+} 
 
 export async function flushUsers() {
-  await redis.del("users");
-}
+  await redis.del("users")
+} 
